942d74c5612a2724dbe19002da38b4cd3e6b378d
Michael Lamph <michael.lamph92@gmail.com>
2020-09-08 16:31:54 +0100

    further updates

--- src/app/credit-rules/credit-rules.component.ts
+++ src/app/credit-rules/credit-rules.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormArray, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'esw-main',
@@ -6,10 +7,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./credit-rules.component.scss']
 })
 export class CreditRulesComponent implements OnInit {
-  constructor() {}
+
+  creditRulesForm: FormGroup
+
+  constructor(
+    private formBuilder: FormBuilder
+    ) {}
 
   ngOnInit() {
 
+    this.initForm();
+
+  }
+
+  private initForm() {
+
+    const countryCreditRules = this.formBuilder.array([]);
+    this.creditRulesForm = this.formBuilder.group({
+        'brand': [0],
+        'countryCreditRules': countryCreditRules }
+      );
+
+  }
+
+  public addCreditRulesRow() {
+    this.getCountryCreditRulesFormArray.push(
+      new FormGroup({
+        'countryIso': new FormControl(null),
+        'creditTypeId': new FormControl(null),
+        'returnFee': new FormControl(null),
+        'isMerchandizeRefundable': new FormControl(null),
+        'isValueAddedServiceRefunadble': new FormControl(null),
+        'isMerchandizeDutyRefundable': new FormControl(null),
+        'isMerchandizeTaxRefundable': new FormControl(null),
+        'iShippingRefundable': new FormControl(null),
+        'isShippingDutyRefundable': new FormControl(null),
+        'isShippingTaxRefundable': new FormControl(null),
+        'isOtherTaxesRefundable': new FormControl(null),
+        'isOtherFeesRefundable': new FormControl(null),
+        'isInternationalServiceFeeRefundable': new FormControl(null),
+        'isAdminFeeAmountRefundable': new FormControl(null)
+      })
+    );
+  }
+
+  get countryCreditRulesControls() {
+    return this.creditRulesForm['controls']['countryCreditRules']['controls'];
+  }
+
+  get getCountryCreditRulesFormArray() {
+    return <FormArray>this.creditRulesForm.get('countryCreditRules');
   }
 
 }
--- src/app/credit-rules/credit-rules.module.ts
+++ src/app/credit-rules/credit-rules.module.ts
@@ -2,11 +2,17 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { CreditRulesComponent } from './credit-rules.component';
 import { CreditRulesRoutingModule } from './credit-rules.routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CardModule } from 'primeng/card';
 
 @NgModule({
   imports: [
     CreditRulesRoutingModule,
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CardModule,
+
   ],
   providers: [],
   declarations: [CreditRulesComponent]
